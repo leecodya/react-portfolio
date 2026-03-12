bfc9171a359c07cb49e696d789fe2c3ab980200b
Cody Lee <lee.codya@gmail.com>
2026-03-11 20:49:25 -0400

    added top bar to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,19 @@ import TechnicalResume from '../assets/Technical-Developer.pdf';
 
 export default function Header() {
     return (
-        <header className='header'>
-            <div className="extended-container">
-                <h1>Cody Lee</h1>
-                <h2>Web Developer & Software Engineer</h2>
-                <h3>I build fast, user-focused websites and applications that solve real business problems</h3>
-                <div className="button-flex">
-                    <button><a href="#projects">View My Work</a></button>
-                    <button><a href={TechnicalResume} download>Download Resume</a></button>
+        <>
+            <div className="top-bar"></div>
+            <header className='header'>
+                <div className="extended-container">
+                    <h1>Cody Lee</h1>
+                    <h2>Web Developer & Software Engineer</h2>
+                    <h3>I build fast, user-focused websites and applications that solve real business problems</h3>
+                    <div className="button-flex">
+                        <button><a href="#projects">View My Work</a></button>
+                        <button><a href={TechnicalResume} download>Download Resume</a></button>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     )
 }
